a9a536c20bddf9674913be030021b4383b2df34f
CodeCriminal365 <120529066+CodeCriminal365@users.noreply.github.com>
2023-01-31 15:26:51 -0800

    Create Game.js

---
+++ src/Game.js
@@ -0,0 +1,13 @@
+Ball.Game = function (game) {};
+Ball.Game.prototype = {
+  create() {},
+  initLevels() {},
+  showLevel(level) {},
+  updateCounter() {},
+  managePause() {},
+  manageAudio() {},
+  update() {},
+  wallCollision() {},
+  handleOrientation(e) {},
+  finishLevel() {},
+};
